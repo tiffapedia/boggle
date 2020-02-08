89e2c4ef9685cf0ee1d910481ec5918609133b61
Tiffany J <tiffapedia@gmail.com>
2020-02-07 20:02:33 -0800

    deploy it on netlify

---
+++ build/precache-manifest.af4e76d8fd08fe16dfe27eaa62f45b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c576285f8999543a8450d01010e2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ab7207ff47196facf0",
+    "url": "/static/css/main.50586854.chunk.css"
+  },
+  {
+    "revision": "d56298e94ad68ec962bc",
+    "url": "/static/js/2.a46d5d0c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a46d5d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ab7207ff47196facf0",
+    "url": "/static/js/main.6f462261.chunk.js"
+  },
+  {
+    "revision": "a9c7bee4a20f21bafcd5",
+    "url": "/static/js/runtime-main.a2d72da2.js"
+  }
+]);
