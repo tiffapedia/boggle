e35f6a5b9724622ec14304fd7c668e281ff1c017
Tiffany J <tiffapedia@gmail.com>
2020-02-09 12:36:32 -0800

    change die color state

--- src/components/die/Die.js
+++ src/components/die/Die.js
@@ -3,16 +3,39 @@ import './Die.css';
 
 class Die extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      isSelected: false,
+      backgroundColor: '#FFFFFF',
+    };
+  }
+
   render() {
     return (
-      <div className='die' onClick={this.handleClick}>
+      <div
+        className='die'
+        onClick={this.handleClick}
+        style={{ backgroundColor: this.state.backgroundColor }}
+      >
         <span>{this.props.die.letter}</span>
       </div>
     )
   }
 
   handleClick = () => {
-    console.log('is clicked: ', this.props.letter);
+    console.log('is clicked: ', this.props.die.letter);
+    if (!this.state.isSelected) {
+      this.setState({
+        isSelected: true,
+        backgroundColor: '#F6DD45',
+      });
+    } else {
+      this.setState({
+        isSelected: false,
+        backgroundColor: '#FFFFFF',
+      })
+    }
   }
 
 }
